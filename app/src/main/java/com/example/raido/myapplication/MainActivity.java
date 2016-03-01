0539a74e308732e0a0f282c411383dde4ab5c3bf
rhallop <raido.hallop@gmail.com>
2016-03-01 21:12:24 +0200

    uuno kommit

--- app/src/main/java/com/example/raido/myapplication/MainActivity.java
+++ app/src/main/java/com/example/raido/myapplication/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void masterBlaster(){
+
+        int j = 1+3;
+    }
 }
